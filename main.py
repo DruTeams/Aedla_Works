a1605d077492c7aa6a01b46155ac026dd29b735c
druteams <andrueldelosanimes@gmail.com>
2023-04-15 18:42:27 -0500

    EliminandoLoInncesario

--- main.py
+++ main.py
@@ -18,12 +18,37 @@ def contact():
     
 
 
+#Series Aedla-World
+
 @app.route('/your_name', methods=['GET','POST'])
 def your_name():
     if request.method == 'POST':
         return ""
     else:
         return render_template('series/Your_Name.html')
+
+
+
+@app.route('/Seishun_Buta_Yarō_wa_Yumemiru_Shōjo_no_Yume_wo_Minai')
+def Seishun_Buta_Yarō_wa_Yumemiru_Shōjo_no_Yume_wo_Minai():
+    if request.method == 'POST':
+        return ""
+    else:
+        return render_template('series/Seishun_Buta_Yarō_wa_Yumemiru_Shōjo_no_Yume_wo_Minai.html')
+
+
+
+@app.route('/Saekano_The_Movie', methods=['GET','POST'])
+def saekano_the_movie():
+    if request.method == 'POST':
+        return ""
+    else:
+        return render_template('series/Saekano_The_Movie.html')
+
+
+
+
+
 
 
 if __name__ == '__main__':
